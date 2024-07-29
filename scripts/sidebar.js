70cb667a12b1b58650b19441562f6c67938207ef
Seth Axen <seth@sethaxen.com>
2024-07-29 11:02:51 +0200

    Clean up interface and add menu bar

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,10 +1,3 @@
 function toggleSidebar() {
-    const sidebar = document.getElementById('sidebar');
-    const isHidden = sidebar.classList.contains('hidden');
-    if (isHidden) {
-        sidebar.style.transform = 'translateX(0)';
-    } else {
-        sidebar.style.transform = 'translateX(-100%)';
-    }
-    sidebar.classList.toggle('hidden');
+    document.getElementById('sidebar').classList.toggle('active');
 }
